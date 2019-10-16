a3e4cd568dc3c37a1174f88c6d8cb2847a5aa2b8
Tyler Biswell <tsbiswell@gmail.com>
2019-10-16 10:56:45 -0600

    implement token on Login

--- database/migrations/20180910095022_users.js
+++ database/migrations/20180910095022_users.js
@@ -1,15 +1,11 @@
 exports.up = function(knex) {
-  return knex.schema.createTable('users', users => {
-    users.increments();
-
-    users
-      .string('username', 128)
-      .notNullable()
-      .unique();
-    users.string('password', 128).notNullable();
+  return knex.schema.table('users', tbl => {
+    tbl.string('role');
   });
 };
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('users');
+exports.down = function(knex) {
+  return knex.schema.table('users', tbl => {
+    tbl.dropColumn('role');
+  });
 };
